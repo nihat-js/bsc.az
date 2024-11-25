ecaa17bbfb73c994fdb54cbc3892f628eb775eba
Nihat <abdullazadenihat@gmail.com>
2024-11-25 12:55:14 +0400

    sanctum added

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function all(){: void
+        
+    }
+
+    public function create(){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\UserAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/",function(){
+    return view("index");
+});
+
+Route::post('register',[UserAuthController::class,'register']);
+Route::post('login',[UserAuthController::class,'login']);
+Route::post('logout',[UserAuthController::class,'logout'])
+  ->middleware('auth:sanctum');
