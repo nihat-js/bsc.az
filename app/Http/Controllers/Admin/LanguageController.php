d8a743cbae18caca2c7e386a01b65b12bb5156d7
Nihat <abdullazadenihat@gmail.com>
2025-01-10 17:43:42 +0400

    language bayraq

--- app/Http/Controllers/Admin/LanguageController.php
+++ app/Http/Controllers/Admin/LanguageController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Language;
+use App\Services\ImageUploadService;
 use Illuminate\Http\Request;
+use Storage;
 
 
 
 
 class LanguageController
 {
+    private $uploadPath = 'uploads/languages/';
     public function all()
     {
         $languages = Language::all();
         return response()->json($languages, 200);
     }
+
 
     public function add(Request $request)
     {
@@ -22,8 +26,16 @@ class LanguageController
             'is_visible' => 'required|boolean',
             'code' => 'required|string|unique:languages,code|max:2',
             'name' => 'required|string|max:255',
+            "image" => "sometimes|string",
         ]);
 
+        if (@$request["image"]) {
+            Storage::disk('public')->makeDirectory($this->uploadPath);
+            $coverImage = ImageUploadService::uploadBase64Image($request["image"], $this->uploadPath);
+            $validated["image"] = $coverImage;
+        }
+
+        // return response()->json(["status" => "ok", "data" => $request->all()], 201);
         $language = Language::create($validated);
 
         return response()->json(["status" => "ok", "data" => $language,], 201);
@@ -51,8 +63,15 @@ class LanguageController
             'is_visible' => 'sometimes|boolean',
             'code' => 'sometimes|string|unique:languages,code,' . $id . '|max:2',
             'name' => 'sometimes|string|max:255',
+            "image" => "sometimes|string",
         ]);
 
+
+        if (@$validated["image"]) {
+            Storage::disk("public")->delete($this->uploadPath . $language->getRawOriginal("image"));
+            $image = ImageUploadService::uploadBase64Image($request["image"], $this->uploadPath);
+            $validated["image"] = $image;
+        }
         $language->update($validated);
 
         return response()->json(["status" => "ok", "data" => $language], 200);
@@ -61,8 +80,16 @@ class LanguageController
     public function delete($id)
     {
         $language = Language::findOrFail($id);
+
+        if ($language->getRawOriginal("image")) {
+            Storage::disk("public")->delete($this->uploadPath . $language->getRawOriginal("image"));
+        }
         $language->delete();
 
-        return response()->json(['status' => 'ok'], 200);
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Language deleted successfully',
+            "data" => $language
+        ], 200);
     }
 }
--- app/Models/Language.php
+++ app/Models/Language.php
@@ -9,26 +9,25 @@ class Language extends Model
 {
     use HasFactory;
 
-    // The table associated with the model.
     protected $table = 'languages';
-
-    // Primary key field (optional, Laravel assumes 'id' by default)
     protected $primaryKey = 'id';
-
-    // The data type of the primary key
     protected $keyType = 'int';
-
-    // The attributes that are mass assignable
     protected $fillable = [
         'is_visible',
         'code',
         'name',
+        'image'
     ];
 
     // The attributes that should be cast to native types
     protected $casts = [
         'is_visible' => 'boolean',  // Cast 'is_visible' to a boolean
     ];
+
+    public function getImageAttribute($value)
+    {
+        return $value ? asset("/storage/uploads/languages/" . $value) : null;
+    }
 
     // Optional: Add the created_at and updated_at if not automatically handled
     public $timestamps = true;
